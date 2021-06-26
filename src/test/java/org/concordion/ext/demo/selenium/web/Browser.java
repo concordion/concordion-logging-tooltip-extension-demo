5efb690e2227f4330483dd2199e4deb7da795126
Nigel Charman <nigel.charman.nz@gmail.com>
2021-06-26 15:18:29 +1200

    Setup Chrome and ChromeDriver for Github Actions

--- src/test/java/org/concordion/ext/demo/selenium/web/Browser.java
+++ src/test/java/org/concordion/ext/demo/selenium/web/Browser.java
@@ -1,18 +1,30 @@
 package org.concordion.ext.demo.selenium.web;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
 import org.concordion.ext.selenium.SeleniumEventLogger;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.support.events.EventFiringWebDriver;
 
 /**
  * Manages the browser session. 
  */
 public class Browser {
+    static {
+        WebDriverManager.chromedriver().setup();
+    }
+
     private WebDriver driver;
     
     public Browser() {
-        driver = new ChromeDriver();
+        ChromeOptions options = new ChromeOptions();
+        if (("true").equals(System.getenv("HEADLESS_CHROME"))) {
+            options.addArguments("--headless");
+            options.addArguments("--no-sandbox");
+            options.addArguments("--disable-dev-shm-usage");
+        }
+        driver = new ChromeDriver(options);
     }
     
     public void close() {
